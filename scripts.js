9683004b6ce58e907515644891642cf499a301e7
Ravi Kumar <ravi244hz@gmail.com>
2024-10-23 22:46:28 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function showFact() {
+    const factElement = document.getElementById('funFact');
+    factElement.style.display = 'block';
+}
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("Welcome to the Indian Street Food web app!");
+});
+
